5c5dadc1b5d4bd23d63552b4c6019b163ffebc76
Nyoman Ari S <nyomanari21@gmail.com>
2026-05-08 15:39:17 +0700

    Add mailto button in hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,6 +29,11 @@ export default function Home() {
               <path fill="currentColor" d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z" />
             </svg>
           </a>
+          <a href="mailto:nyomanari21@gmail.com" className="hover:text-blue-500 transition">
+            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m0 4l-8 5l-8-5V6l8 5l8-5z" />
+            </svg>
+          </a>
         </div>
 
         {/* Tombol Call to Action (CTA) */}
